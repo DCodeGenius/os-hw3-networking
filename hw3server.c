affa93475b47e003601ecfdbab9788b54aee43cf
DCodeGenius <daniel.hlprin22@gmail.com>
2025-12-26 20:43:23 +0200

    put main in the hw3server in order to compile

--- hw3server.c
+++ hw3server.c
@@ -1,4 +1,12 @@
 int main(int argc, char **argv)
 {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s port\n", argv[0]);
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+
+    // later: server_run(port);
     return 0;
 }
